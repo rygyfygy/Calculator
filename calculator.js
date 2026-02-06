198f1552fc3954191f359965a578d9b96119fa78
rygyfygy <237001230+rygyfygy@users.noreply.github.com>
2026-02-06 22:06:30 +0100

    The dot now can be input only one time per value.

--- calculator.js
+++ calculator.js
@@ -27,57 +27,56 @@ function clearAll() {
 
 
 //calculate
-function operate(operator, number1, number2){
+function operate(operator, number1, number2) {
     const num1 = parseFloat(number1);
     const num2 = parseFloat(number2);
-    switch (operator){
-        case '+': 
+    switch (operator) {
+        case '+':
             return num1 + num2;
-        case '-': 
+        case '-':
             return num1 - num2;
-        case '*': 
+        case '*':
             return num1 * num2;
-        case '/': 
+        case '/':
             return num1 / num2;
     }
 }
 
-/*
-function calculate() {
-    display.value = eval(display.value)
-}
-*/
+//// BUTTON - ACTION ASSIGNMENT
+//  num
+const buttons = Array.from(document.querySelectorAll('.num'));
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => input_number(event.target));
+});
 
-// button - action assignment
-    //  num
-    const buttons = Array.from(document.querySelectorAll('.num'));
-    buttons.forEach(button => {
-        button.addEventListener('click', (event) => input_number(event.target)); 
-    });
+// dot
+const dot = document.querySelector('#dot');
+dot.addEventListener('click', (event) => { if (!display.value.includes('.')) { input_number(event.target); } });
 
-    //  clear
-    const AC = document.querySelector('#clear');
-    AC.addEventListener('click', clearAll);
-    
 
-    //  equal
-    const operators = document.querySelectorAll('.operator');
-    operators.forEach((operatorButton) => operatorButton.addEventListener('click', () => {
-        if (calcFirstNumber === null) {
-            calcFirstNumber = display.value;
-            clearDisplay();
-        } 
-        calcOperator = operatorButton.textContent;
-    }));
+//  clear
+const AC = document.querySelector('#clear');
+AC.addEventListener('click', clearAll);
 
-    const eq = document.querySelector('#equal');
-    eq.addEventListener('click', () => {
-        if (calcOperator && calcFirstNumber) {
-            calcSecondNumber = display.value;
-            display.value = operate(calcOperator, calcFirstNumber, calcSecondNumber);
-            clearCache();
-            calcFirstNumber = parseint(display.value);
-        }});
+//  equal
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operatorButton) => operatorButton.addEventListener('click', () => {
+    if (calcFirstNumber === null) {
+        calcFirstNumber = display.value;
+        clearDisplay();
+    }
+    calcOperator = operatorButton.textContent;
+}));
+
+const eq = document.querySelector('#equal');
+eq.addEventListener('click', () => {
+    if (calcOperator && calcFirstNumber) {
+        calcSecondNumber = display.value;
+        display.value = operate(calcOperator, calcFirstNumber, calcSecondNumber);
+        clearCache();
+        calcFirstNumber = parseInt(display.value);
+    }
+});
 
 
 
